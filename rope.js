8abb698cd89135c2da1604322238c5dd1be3c181
XxatharvaxX <86345529+XxatharvaxX@users.noreply.github.com>
2021-08-25 20:26:30 +1200

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,20 @@
+class rope{
+	constructor()
+	{
+		this.pointA = pointA;
+		this.pointB = pointB;
+
+		var options={
+			bodyA:body1,
+			bodyB:body2,
+			pointB:{x:this.pointA, y:this.pointB}
+		}
+
+	//create rope constraint here
+
+	}
+
+
+    //create display() here 
+
+}
